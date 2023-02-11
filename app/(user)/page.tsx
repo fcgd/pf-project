ad1a9bc1bb950372b6c5ee7501faea53831017cf
Fabricio C <fabriciocesaretti@gmail.com>
2023-02-11 13:34:24 -0300

    Preview Mode

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,9 +1,17 @@
 import "../globals.css";
+import { previewData } from "next/headers";
 
 export default function Home() {
+  if (previewData()) {
+    return (
+      <main className="bg-black h-screen flex text-center">
+        <h1 className="text-white m-auto text-4xl">Preview Mode</h1>
+      </main>
+    );
+  }
   return (
     <main className="bg-black h-screen flex text-center">
-      <h1 className="text-white m-auto text-4xl">Hello</h1>
+      <h1 className="text-white m-auto text-4xl">Not in Preview Mode</h1>
     </main>
   );
 }
